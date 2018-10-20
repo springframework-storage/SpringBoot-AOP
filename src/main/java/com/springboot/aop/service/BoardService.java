0cabe19f966a83c5f79063429c2770ad165984c9
MinGOODdev <mingood92@gmail.com>
2018-10-21 02:41:44 +0900

    Feat: AOP '@Around'

--- src/main/java/com/springboot/aop/service/BoardService.java
+++ src/main/java/com/springboot/aop/service/BoardService.java
@@ -1,27 +1,11 @@
 package com.springboot.aop.service;
 
 import com.springboot.aop.domain.Board;
-import com.springboot.aop.repository.BoardRepository;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Slf4j
-@Service
-public class BoardService {
+public interface BoardService {
 
-  @Autowired
-  private BoardRepository boardRepository;
-
-  public List<Board> getBoards() {
-    long start = System.currentTimeMillis();
-    List<Board> boards = boardRepository.findAll();
-    long end = System.currentTimeMillis();
-
-    log.info("*** 수행 시간: {}", end - start);
-    return boards;
-  }
+  List<Board> getBoards();
 
 }
